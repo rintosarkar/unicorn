1fddc18aae99403c50f46e593a33950311a5ca4b
Rinto <rinto.sp@gmail.com>
2015-03-27 01:47:20 -0700

    First commit for Unicorn

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>unicorn</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700' rel='stylesheet' type='text/css'>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/conCustom.css">
+</head>
+<body>
+	<div class="uni-header">
+		<div class="container">
+			<div class="uni-header-top">
+				<div class="row">
+					<div class="col-sm-7">
+						<p class="uni-title">UNICORN</p>
+					</div>
+					<div class="col-sm-5">
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">HOME</a></li>
+								<li><a href="#">ABOUT</a></li>
+								<li><a href="#">GALLERY</a></li>
+								<li><a href="#">HELLO</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div> <!-- uni-header-top -->
+			<div class="uni-header-mid">
+				<div class="row">
+					<div class="col-sm-12">
+						<h1 class="uni-main-title">STORYTELLER</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-5 col-sm-offset-7">
+						<p class="uni-title-details">
+							One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. 
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="uni-header-bottom">
+				<div class="row">
+					<div class="col-sm-3 col-sm-offset-9">
+						<div class="uni-btn">
+							read the rest
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="uni-whoWhy">
+		<div class="container">
+			<div class="row">
+				<div class="uni-ww-top">
+					<div class="col-sm-12">
+						<span><img src="img/intro.png"></span>
+						<p class="uni-intro-title">Who & Why</p>
+						<p class="uni-intro-details">
+							The gentlemen who rented the room would sometimes take their evening meal at home in the living room that was used by everyone, and so the door to this room was often kept closed in the evening. But Gregor found it easy to give up having the door open, he had, after all, often failed to make use of it when it was open and, without the family having noticed it, lain in his room in its darkest corner. One time, though, the charwoman left the door.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row"></div>
+		</div>
+	</div>
+	<div class="uni-service">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<span class="uni-wow-feature"><img src="img/clock.png"></span>
+					<p class="wow-title">An Wow Feature</p>
+					<p class="wow-details">
+						The gentlemen who rented the room would sometimes take their evening meal at home in the living.
+					</p>
+				</div>
+				<div class="col-sm-4">
+					<span class="uni-wow-feature"><img src="img/beautiful.png"></span>
+					<p class="wow-title">A Beautiful Feature</p>
+					<p class="wow-details">
+						The gentlemen who rented the room would sometimes take their evening meal at home in the living.
+					</p>
+				</div>
+				<div class="col-sm-4">
+					<span class="uni-wow-feature"><img src="img/landw.png"></span>
+					<p class="wow-title">An Amazing Feature</p>
+					<p class="wow-details">
+						The gentlemen who rented the room would sometimes take their evening meal at home in the living.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
