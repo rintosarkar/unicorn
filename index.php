91dbeade23b2b751fd5553782894e577abf6db84
Rinto <rinto.sp@gmail.com>
2015-03-28 01:36:20 -0700

    third commit from home

--- index.php
+++ index.php
@@ -131,6 +131,45 @@
 				</div>
 			</div>
 		</div>
+	</div> <!-- Featured section end -->
+	<div class="uni-client">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+						<!-- indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="myCarousel" data-slide-to="0"></li>
+							<li data-target="myCarousel" data-slide-to="1"></li>
+							<li data-target="myCarousel" data-slide-to="2"></li>
+							<li data-target="myCarousel" data-slide-to="3"></li>
+						</ol>
+						<!-- Wrapper for slider -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="img/clients.png" alt="clients">
+								<div class="carousel-caption">
+									<h3 class="uni-caption">CLIENTS</h3>
+								</div>	
+							</div>
+							<div class="item">
+								<img src="img/clients01.png" alt="clients01">
+								<div class="carousel-caption">
+									<h3 class="uni-caption">CLIENTS</h3>
+								</div>
+							</div>
+						</div>
+					</div>
+					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="false"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="false"></span>
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
